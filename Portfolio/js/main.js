390f4e251d85a568575559644ae5b2c821111618
rasanelpaulcayanan <55368213+rasanelpaulcayanan@users.noreply.github.com>
2020-09-17 21:59:31 +0800

    sad

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -1,6 +1,8 @@
-const links = [
+var links = [
     {
-        label:"Week 1 Notes";
-        url:
+        label:'Week 1 Notes',    
+        link:'https://github.com/rasanelpaulcayanan/WDD330/tree/master/Portfolio/Week1'
     }
 ]
+document.getElementById("dynamicContent").innerHTML = links[0].label;
+document.getElementById("dynamicContent2").innerHTML = links[0].link;
