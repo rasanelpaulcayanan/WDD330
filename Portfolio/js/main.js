5fef7c8104c7b1d4b747e61b2db73c6da3a448bf
rasanelpaulcayanan <55368213+rasanelpaulcayanan@users.noreply.github.com>
2020-09-18 07:08:27 +0800

    added javscript elements

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -1,8 +1,7 @@
 var links = [
     {
-        label:'Week 1 Notes',    
+        label:'Week 1',    
         link:'https://github.com/rasanelpaulcayanan/WDD330/tree/master/Portfolio/Week1'
     }
 ]
 document.getElementById("dynamicContent").innerHTML = links[0].label;
-document.getElementById("dynamicContent2").innerHTML = links[0].link;
