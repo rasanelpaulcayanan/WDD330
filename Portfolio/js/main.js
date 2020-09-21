32aef7f05a9dbae681b1df2b811c1f528adae3b0
rasanelpaulcayanan <55368213+rasanelpaulcayanan@users.noreply.github.com>
2020-09-21 11:03:44 +0800

    updated links

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -1,9 +1,32 @@
-var links = [
+const links = [
     {
         label:'Week 1',    
         link:'Week1/index.html'
     }
+,
+    {
+        label:'Week 2',    
+        link:'Week2/index.html'
+    }
 ]
-document.getElementById("dynamicContent").innerHTML = links[0].label;
-document.getElementById("dynamicContent2").href = links[0].link;
 
+
+    //document.getElementById("dynamicContent").innerHTML = links[i].label;
+   // document.getElementById("dynamicContent2").href = links[i].link;
+
+   const ol = document.getElementById('list');
+
+   for ( i = 0, max = links.length; i < max ; i++)
+   {
+       const li = document.createElement('li');
+       const a = document.createElement('a');
+      // li.innerHTML = links[i].label;
+       a.href = links[i].link;
+       a.innerHTML = links[i].label
+       ol.appendChild(li);
+       li.appendChild(a);
+      
+   
+   }
+   
+   
